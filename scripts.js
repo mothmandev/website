c2a7bc8a39d3d44ea2e5c2bc5523275de2497f50
Simon From Jakobsen <sfja.skp@edu.mercantec.dk>
2021-12-10 12:27:10 +0100

    init

---
+++ scripts.js
@@ -0,0 +1,27 @@
+
+const pad = (v) => {
+    const s = v.toString();
+    return s.length < 2 ? '0' + s : s;
+}
+
+const setClock = (htmltarget) => {
+    const date = new Date();
+    const formatedTime = `${pad(date.getHours())}:${pad(date.getMinutes())}:${pad(date.getSeconds())}`;
+    const formattedDate = `${pad(date.getDate())}/${pad(date.getMonth()+1)}/${pad(date.getFullYear())}`;
+    htmltarget.textContent = `${formattedDate} ${formatedTime}`;
+}
+
+const setClockHandler = () => {
+    const clock = document.querySelector('#topbar-clock');
+    const loop = () => {
+        setClock(clock);
+        window.requestAnimationFrame(loop);
+    }
+    window.requestAnimationFrame(loop);
+}
+
+
+const main = () => {
+    setClockHandler();
+}
+main();
